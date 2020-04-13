07c379ad659ddb5f2b19459f9fd2e9d5c3b0f155
Hugo Vargas <vargas88hugo@gmail.com>
2020-04-13 14:53:42 -0500

    [Feature] Created DTO and implemented persistence with database on signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
-    private userRepositoru: UserRepository,
+    private userRepository: UserRepository,
   ) {}
+
+  signUp(authCredentialsDto: AuthCredentialsDto) {
+    return this.userRepository.signUp(authCredentialsDto);
+  }
 }
