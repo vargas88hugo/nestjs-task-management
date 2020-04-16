ad8f700d78cfdb5e82c6369e5aae491e49eb9cb7
Hugo Vargas <vargas88hugo@gmail.com>
2020-04-15 19:51:44 -0500

    [Update] Implemented get task by id with authentication

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -17,8 +17,10 @@ export class TasksService {
     return this.taskRepository.getTasks(filterDto, user);
   }
 
-  async getTaskById(id: number): Promise<Task> {
-    const found = await this.taskRepository.findOne(id);
+  async getTaskById(id: number, user: User): Promise<Task> {
+    const found = await this.taskRepository.findOne({
+      where: { id, userId: user.id },
+    });
 
     if (!found) {
       throw new NotFoundException(`Task with ID ${id} not found`);
@@ -39,11 +41,11 @@ export class TasksService {
     }
   }
 
-  async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
-    const task = await this.getTaskById(id);
-    task.status = status;
-    await task.save();
+  // async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
+  //   const task = await this.getTaskById(id);
+  //   task.status = status;
+  //   await task.save();
 
-    return task;
-  }
+  //   return task;
+  // }
 }
